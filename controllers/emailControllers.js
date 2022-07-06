9bff3963c0dd9a3ca8533683d015eae8daf1b172
Arnold-O <arnoldoghiator@gmail.com>
2022-07-06 19:54:34 +0100

    nodemailer functions

--- controllers/emailControllers.js
+++ controllers/emailControllers.js
@@ -1,4 +1,5 @@
 const catchAsyncWares = require("../middlewares/catchAsyncWares");
+const nodemailer = require('nodemailer')
 
 
 
@@ -7,7 +8,20 @@ const catchAsyncWares = require("../middlewares/catchAsyncWares");
 
 
 exports.sendEmail = catchAsyncWares(async(req,res,next)=>{
+    // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
 
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'alize.considine42@ethereal.email',
+        pass: 'PcvTYEEDpbawQZYmyw'
+    }
+});
+
+ley
 
 
     res.json({
